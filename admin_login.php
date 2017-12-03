c740b119ee0488ac409a2d2574f7241a5e491d4f
Ben Chomsang <schomsan@u.rochester.edu>
2017-12-02 21:15:34 -0500

    added placeholder for password field on Login page

--- admin_login.php
+++ admin_login.php
@@ -100,8 +100,8 @@
 			<!--login form stuff-->
 			<form method="post" action="admin_login.php">
 			<div>
-			<input type="text" name="username" class="username" placeholder="username" />
-			<input type="password" class="password" name="password"/>
+			<input type="text" name="username" class="username" placeholder="Username" />
+			<input type="password" class="password" name="password" placeholder="Password"/>
 			<button type="submit" id="submit">Log In</button>
 			</div>
 			</form>
